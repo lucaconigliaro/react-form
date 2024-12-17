f5a1d4e07ad2bee53fde059d4b3b5a8ac5776911
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-17 17:11:40 +0100

    input test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
+import { useState } from "react";
 
 function App() {
+  
+  const [articleTitle, setArticleTitle] = useState("");
 
   return (
     <>
-      
+      <div className="container">
+        <h1 className="text-center mb-3">New Blog Articles</h1>
+
+        <form className="">
+          <label htmlFor="articleTitle">Nome dell'articolo</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Inserisci il titolo dell'articolo"
+            value={articleTitle}
+            onChange={(event) => setArticleTitle(event.target.value)}
+          />
+        </form>
+        <p>Test: {articleTitle}</p>
+      </div>
     </>
   )
 }
